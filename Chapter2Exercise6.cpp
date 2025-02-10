f8c07b1458396fa52f220343491f42638f9e8577
Joshuathomas15 <jjulyo6mdls958@gmail.com>
2025-02-09 19:48:44 -0500

    Add project files.

---
+++ Chapter2Exercise6.cpp
@@ -0,0 +1,12 @@
+// Chapter2Exercise6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+double payamount = 2200.0;
+double payperloads = 26;
+int annualpay = (payamount*payperloads);
+int print(annualpay);
+_cout << "annualpay   =$<< annualpay<<endl;
+return 0;
